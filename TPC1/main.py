e6b41715348d60e09db620565e9ac826fd91033f
ritateixeira89494 <a89494@alunos.uminho.pt>
2023-02-20 00:15:48 +0000

    TPC1

---
+++ TPC1/main.py
@@ -0,0 +1,17 @@
+import re
+
+def read():
+    file = open("myheart.csv", "r")
+    for i in range(1):
+        file, next(file)
+
+    for line in file:
+
+
+
+def main():
+    print("hello")
+
+
+if __name__ == '__main__':
+    main()
